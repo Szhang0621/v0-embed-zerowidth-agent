cf9d289c9e90657432094f6dbb1535132e01e8ae
Szhang0621 <shiyuz@cca.edu>
2025-06-05 02:04:27 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -15,7 +15,7 @@ const chatConfig = {
   flowURL:
     "https://api.zerowidth.ai/v1/process/OCWi9PTGrb7OPwZ6FBbp/hAubXpDvbd2FVtSMJF8P",
   header: {
-    title: "curious?chat with AI me ;)",
+    title: "curious? chat with AI me ;)",
   },
   suggestedPromptsTitle: "Try ask",
   suggestedPrompts: [
